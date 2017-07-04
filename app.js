dcc38046cfa69472f48682dcf8c288b2c4cbdfec
Gabriel Souza <gabrie.ssouza@gmail.com>
2017-07-03 23:36:29 -0300

    Trabalhando na visualização

--- app.js
+++ app.js
@@ -1,44 +1,75 @@
 const express = require('express'),
-  bodyParser = require('body-parser'),  
   path = require('path'),
   app = express(),
-  port = process.env.PORT || 3000;
+  five = require('johnny-five'),
+  board = new five.Board(),
+  io = require('socket-io'),
+  port = process.env.PORT || 8080;
 
-
+/*
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-
+*/
 
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
 
-
-app.get('/', (req, res)=>{
-    res.send('Home');
-});
-
-app.get('/temperature', (req, res)=>{
-    res.send('Temperatura');
-});
-
-app.get('/level', (req,res)=>{
-    res.send('Nível');
-});
-
-app.get('/informations', (req, res)=>{
-    res.send('Informações Técnicas');
-});
+board.on('ready', ()=>{
+  //config    
+  let tempSensor = new five.Thermometer({
+    controller: 'LM35',
+    pin: 'A0'
+  });
+  let [valveUp, valveDown] = [new five.Led(13), new five.Led(12)];
 
 
-app.use(function(req, res, next) {
-  res.status = 404;
-  res.send("Not Found!");
-});
 
-app.listen(port, function () {
-  console.log('Server is running on PORT: ' + port);
-});
+/** WEBSOCKET */
+io.on('connection', (socket)=>{
+  socket.on('valveUp', (data)=>{
+    valveUp.on();
+    valveDown.off();
+  });
+  socket.on('valveDown', (data)=>{
+    valveUp.off();
+    valveDown.on();
+  });
+  socket.on('valveMaintain', (data)=>{
+    valveUp.off();
+    valveDown.off();
+  });
+})
+/** ROUTES */
+  app.get('/', (req, res)=>{
+      res.redirect('/home');
+  });
+  app.get('/home', (req, res)=>{
+      res.render('home.pug');
+  });
+  app.get('/temperature', (req, res)=>{
+      res.render('temperature.pug');
+  });
+  app.get('/level', (req,res)=>{
+      res.render('level.pug');
+  });
+  app.get('/informations', (req, res)=>{
+      res.render('informations.pug');
+  });
+  app.use(function(req, res, next) {
+    res.status = 404;
+    res.send("Not Found!");
+  });
+  
+
+  /** LISTEN */
+  app.listen(port, function () {
+    console.log('Server is running on PORT: ' + port);
+  });
+
+})
+
+
 
 
